ce5afe7c4f7cdd72e30062b29f0680f49cb1179d
aeesh <adamaisha352@gmail.com>
2021-10-06 13:57:00 +0100

    font family-theme added

--- UI/src/theme.js
+++ UI/src/theme.js
@@ -1,8 +1,8 @@
-// import { createTheme, adaptV4Theme } from '@mui/material/styles';
-// const theme = createTheme(adaptV4Theme({
-//    typography: {
-//     fontFamily: "Poppins"
-//    }
-// }));
+import { createTheme, adaptV4Theme } from '@mui/material/styles';
+const theme = createTheme(adaptV4Theme({
+   typography: {
+    fontFamily: "Poppins, Inter"
+   }
+}));
 
-// export default theme;
+export default theme;
