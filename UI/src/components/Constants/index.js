332daef26acddbea7f0cf58341bb34342e2eb269
aeesh <adamaisha352@gmail.com>
2021-10-22 13:06:23 +0100

    resolved import error

--- UI/src/components/Constants/index.js
+++ UI/src/components/Constants/index.js
@@ -6,12 +6,12 @@ import kUSD from "../../assets/kUSD.svg";
 import plenty from "../../assets/plenty.svg";
 import USDtz from "../../assets/USDtz.svg";
 import XTZ from "../../assets/XTZ.svg";
-import { FToken } from 'tezoslendingplatformjs';
+import { TezosLendingPlatform } from 'tezoslendingplatformjs';
 
 export const tokens = [
   { title: 'BTCtz', logo: BTCtz, banner: 'BTCtz'},
   { title: 'ETHtz', logo: Ethtz, banner: 'ETH Tez' },
-  { title: 'XTZ', logo: XTZ, banner: 'Tez', assetType: FToken.AssetType.XTZ },
+  { title: 'XTZ', logo: XTZ, banner: 'Tez', assetType: TezosLendingPlatform.AssetType.XTZ },
   { title: 'kUSD', logo: kUSD, banner: 'kUSD' },
   { title: 'PLENTY', logo: plenty, banner: 'PLENTY' },
   { title: 'CTez', logo: ctez, banner: 'CTez'},
