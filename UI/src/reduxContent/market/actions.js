a12796a6d8e7368438c6c6302de8f3f19d626046
aeesh <adamaisha352@gmail.com>
2021-11-11 01:33:09 +0100

    commented code due to undefined protocol address

--- UI/src/reduxContent/market/actions.js
+++ UI/src/reduxContent/market/actions.js
@@ -5,27 +5,27 @@ import {TezosLendingPlatform} from 'tezoslendingplatformjs';
 import {tokens} from '../../components/Constants';
 
 
-export const marketAction = (protocolAddresses)=> async (dispatch) => {
-  const markets = await TezosLendingPlatform.GetMarkets(protocolAddresses);
+export const marketAction = (comptroller, protocolAddresses, server)=> async (dispatch) => {
+  // const markets = await TezosLendingPlatform.GetMarkets(comptroller, protocolAddresses, server);
   const supplyMarkets =  JSON.parse(JSON.stringify(tokens));
   const borrowMarkets =  JSON.parse(JSON.stringify(tokens));
 
-  Object.keys(markets).map((market)=>{
-    supplyMarkets.map((supplyMarket)=>{
-      if(supplyMarket.assetType.toLowerCase() !== undefined && market.toLowerCase() !== undefined){
-        if(market.toLowerCase() === supplyMarket.assetType.toLowerCase()) {
-          supplyMarket.apy = markets[market].supply.rate
-        }
-      }
-    })
-    Object.keys(markets).map((market)=>{
-      borrowMarkets.map((borrowMarket)=>{
-        if(market.toLowerCase() === borrowMarket.assetType.toLowerCase()) {
-          borrowMarket.apy = markets[market].borrow.rate
-        }
-      })
-    })
-  })
+  // Object.keys(markets).map((market)=>{
+  //   supplyMarkets.map((supplyMarket)=>{
+  //     if(supplyMarket.assetType.toLowerCase() !== undefined && market.toLowerCase() !== undefined){
+  //       if(market.toLowerCase() === supplyMarket.assetType.toLowerCase()) {
+  //         supplyMarket.apy = markets[market].supply.rate
+  //       }
+  //     }
+  //   })
+  //   Object.keys(markets).map((market)=>{
+  //     borrowMarkets.map((borrowMarket)=>{
+  //       if(market.toLowerCase() === borrowMarket.assetType.toLowerCase()) {
+  //         borrowMarket.apy = markets[market].borrow.rate
+  //       }
+  //     })
+  //   })
+  // })
 
   const marketData = {supplyMarkets, borrowMarkets}
   dispatch({ type: GET_MARKET_DATA, payload: marketData });
