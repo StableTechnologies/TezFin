d68e8171f4f9251bb35f22bb625710bf342b42c4
itamar <itamar.reif@gmail.com>
2021-11-13 17:14:51 -0500

    granadanet harbinger

--- deploy/compile_targets/CompileTestData.py
+++ deploy/compile_targets/CompileTestData.py
@@ -2,6 +2,7 @@ import smartpy as sp
 
 UTILS = sp.io.import_script_from_url("file:deploy/compile_targets/Utils.py")
 CFG = sp.io.import_script_from_url("file:deploy/compile_targets/Config.py")
+# use granadanet harbinger instead of mock
 # Oracle = sp.io.import_script_from_url("file:deploy/test_data/PriceOracle.py")
 FA12 = sp.io.import_script_from_url("file:deploy/test_data/FA1.2.py")
 FA2 = sp.io.import_script_from_url("file:deploy/test_data/FA2.py")
