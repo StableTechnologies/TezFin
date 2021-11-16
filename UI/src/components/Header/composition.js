0c3fc15d5e27b0972ef3d7942bb1ca3f57d2f332
aeesh <adamaisha352@gmail.com>
2021-11-16 13:29:46 +0100

    added market compositon component

---
+++ UI/src/components/Header/composition.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import Grid from '@mui/material/Grid';
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+
+import CustomizedProgressBars from './progressBar';
+import { useStyles } from './style';
+
+const Composition = (props) => {
+  const classes = useStyles();
+  const {title, data, dataIcon,dataTitle, dataLimitIcon, dataLimitTitle, gridClass,boxClass} = props
+  return (
+    <Grid item xs={12} md={6} className={gridClass}>
+      <Typography> {title} </Typography>
+          <Box className={classes.progressBar}>
+            <CustomizedProgressBars />
+          </Box>
+          <Box sx={{ paddingTop: '55px' }} className={boxClass}>
+            <Grid container>
+              <Grid container item xs={12} sm={5} lg={3} className={classes.borderRight}>
+                <Grid item>
+                    <img src={dataIcon} alt="borrowing-icon" />
+                </Grid>
+                <Grid item>
+                  <Box sx={{paddingLeft: '1rem'}}>
+                    <Typography className={classes.statsTitle}> {dataTitle} </Typography>
+                    <Typography className={classes.statsValue}>${data.totalUsdValue || "0.00"}</Typography>
+                  </Box>
+                </Grid>
+              </Grid>
+              <Grid item sm={1}></Grid>
+              <Grid container item sm={6} lg={4}>
+                <Grid item>
+                    <img src={dataLimitIcon} alt="borrowLimit-Icon" />
+                </Grid>
+                <Grid item>
+                  <Box sx={{paddingLeft: '1rem'}}>
+                    <Typography className={classes.statsTitle}> {dataLimitTitle} </Typography>
+                    <Typography className={classes.statsValue}>${(data.collateral || data.Limit) || "0.00"}</Typography>
+                  </Box>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Box>
+    </Grid>
+  )
+}
+
+export default Composition;
--- UI/src/components/Header/index.js
+++ UI/src/components/Header/index.js
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {NavLink} from "react-router-dom";
 
 import Grid from '@mui/material/Grid';
 import { Typography } from '@mui/material';
-import Box from '@mui/material/Box';
 
-import CustomizedProgressBars from './progressBar';
 import Nav from './nav';
+import Composition from './composition';
 
 import {HeaderCon, classes1, Title, useStyles} from "./style";
 import supplyingIcon from '../../assets/supplyingIcon.svg';
@@ -50,72 +48,25 @@ const Header = () => {
         </Grid>
       </Grid>
       <Grid container>
-        <Grid item xs={12} md={6} className={classes.padding100}>
-          <Typography>Supply Composition</Typography>
-          <Box className={classes.progressBar}>
-            <CustomizedProgressBars />
-          </Box>
-          <Box sx={{ paddingTop: '55px' }} className={classes.box}>
-            <Grid container>
-              <Grid container item xs={12} sm={5} lg={3} className={classes.borderRight}>
-                <Grid item>
-                    <img src={supplyingIcon} alt="supplying-Icon" />
-                </Grid>
-                <Grid item>
-                  <Box sx={{paddingLeft: '1rem'}}>
-                    <Typography className={classes.statsTitle}>Supplying</Typography>
-                    <Typography className={classes.statsValue}>${supplyComposition.totalUsdValue}</Typography>
-                  </Box>
-                </Grid>
-              </Grid>
-              <Grid item sm={1}></Grid>
-              <Grid container item xs={12} sm={6} lg={4}>
-                <Grid item>
-                    <img src={collateralizedIcon} alt="collateralized-Icon" />
-                </Grid>
-                <Grid item>
-                  <Box sx={{paddingLeft: '1rem'}}>
-                    <Typography className={classes.statsTitle}>Collateralized</Typography>
-                    <Typography className={classes.statsValue}>${supplyComposition.collateral}</Typography>
-                  </Box>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={6} className={classes.padding30}>
-          <Typography>Borrow  Limit</Typography>
-          <Box className={classes.progressBar}>
-            <CustomizedProgressBars />
-          </Box>
-          <Box sx={{ paddingTop: '55px' }}>
-            <Grid container>
-              <Grid container item sm={5} lg={3} className={classes.borderRight}>
-                <Grid item>
-                    <img src={borrowingIcon} alt="borrowing-icon" />
-                </Grid>
-                <Grid item>
-                  <Box sx={{paddingLeft: '1rem'}}>
-                    <Typography className={classes.statsTitle}>Borrowing</Typography>
-                    <Typography className={classes.statsValue}>${borrowComposition.totalUsdValue}</Typography>
-                  </Box>
-                </Grid>
-              </Grid>
-              <Grid item sm={1}></Grid>
-              <Grid container item sm={6} lg={4}>
-                <Grid item>
-                    <img src={borrowLimitIcon} alt="borrowLimit-Icon" />
-                </Grid>
-                <Grid item>
-                  <Box sx={{paddingLeft: '1rem'}}>
-                    <Typography className={classes.statsTitle}>Borrow limit</Typography>
-                    <Typography className={classes.statsValue}>${borrowComposition.borrowLimit}</Typography>
-                  </Box>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Box>
-        </Grid>
+        <Composition
+          title = "Supply Composition"
+          data = {supplyComposition}
+          dataIcon = {supplyingIcon}
+          dataTitle = "Supplying"
+          dataLimitIcon = {collateralizedIcon}
+          dataLimitTitle = "Collateralized"
+          boxClass={classes.box}
+          gridClass={classes.padding100}
+        />
+        <Composition
+          title = "Borrow  Limit"
+          data = {borrowComposition}
+          dataIcon = {borrowingIcon}
+          dataTitle = "Borrowing"
+          dataLimitIcon = {borrowLimitIcon}
+          dataLimitTitle = "Borrow limit"
+          gridClass={classes.padding30}
+        />
       </Grid>
     </HeaderCon>
   )
