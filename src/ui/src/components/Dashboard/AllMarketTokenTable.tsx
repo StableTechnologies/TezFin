e15f053662a8e33edc35c6462df1c9e6d8b14a98
aeesh <adamaisha352@gmail.com>
2022-01-20 13:57:15 +0100

    whitespace

--- src/ui/src/components/Dashboard/AllMarketTokenTable.tsx
+++ src/ui/src/components/Dashboard/AllMarketTokenTable.tsx
@@ -31,62 +31,62 @@ const AllMarketTokenTable = (props) => {
 	};
 
 	return (
-			<TableContainer className={`${classes.root} ${classes.tableCon}`}>
-         {tokenDetails && (
-            <AllMarketModal
-              open={openMktModal}
-              close={closeModal}
-              tokenDetails={tokenDetails}
-            />)}
-				<Table>
-					<TableHead>
-						<TableRow>
-							<TableCell> Token </TableCell>
-							<TableCell align="right"> Market Size </TableCell>
-							<TableCell align="right"> Total Borrowed </TableCell>
-							<TableCell align="right"> Supply APY </TableCell>
-							<TableCell align="right"> Borrow APY </TableCell>
-							<TableCell align="right"> Wallet </TableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{tableData && tableData.map((data) => (
-							<TableRow key={data.title} onClick={(event) => handleClickMktModal(data, event)}>
-								<TableCell>
-									<img src={data.logo} alt={`${data.title}-Icon`} className={classes.img} />
-									<Typography className={classes.tokenName}> {" "} {data.title} </Typography>
-								</TableCell>
-								<TableCell align="right">
-									<span>
-										{(data.marketSize > 0) ? decimalify(data.marketSize.toString(), decimals[data.title]) : "0"} {" "} {data.title}
-									</span> <br/>
-									<span className={classes.faintFont}>
-										${(data.marketSize > 0) ? decimalify((data.marketSize * data.usdPrice).toString(), decimals[data.title]) : "0.00"}
-									</span>
-								</TableCell>
-								<TableCell align="right">
-									<span>
-                    {(data.totalBorrowed > 0) ? decimalify(data.totalBorrowed.toString(), decimals[data.title]) : "0"} {" "} {data.title}
-									</span> <br/>
-									<span className={classes.faintFont}>
-										${(data.totalBorrowed > 0) ? decimalify((data.totalBorrowed * data.usdPrice).toString(), decimals[data.title]) : "0.00"}
-									</span>
-								</TableCell>
-								<TableCell align="right"> {(data.supplyRate > 0) ? Number(data.supplyRate).toFixed(2) : "0"}% </TableCell>
-								<TableCell align="right"> {(data.borrowRate > 0) ? Number(data.borrowRate).toFixed(2) : "0"}% </TableCell>
-								<TableCell align="right">
-									<span>
-										{(data.walletBalance > 0) ? decimalify(data.walletBalance.toString(), decimals[data.title]) : "0"} {data.title}
-									</span> <br/>
-									<span className={classes.faintFont}>
-                    ${(data.walletBalance > 0) ? decimalify((data.walletBalance * data.usdPrice).toString(), decimals[data.title]) : "0.00"}
-									</span>
-								</TableCell>
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-			</TableContainer>
+    <TableContainer className={`${classes.root} ${classes.tableCon}`}>
+      {tokenDetails && (
+        <AllMarketModal
+          open={openMktModal}
+          close={closeModal}
+          tokenDetails={tokenDetails}
+        />)}
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell> Token </TableCell>
+            <TableCell align="right"> Market Size </TableCell>
+            <TableCell align="right"> Total Borrowed </TableCell>
+            <TableCell align="right"> Supply APY </TableCell>
+            <TableCell align="right"> Borrow APY </TableCell>
+            <TableCell align="right"> Wallet </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {tableData && tableData.map((data) => (
+            <TableRow key={data.title} onClick={(event) => handleClickMktModal(data, event)}>
+              <TableCell>
+                <img src={data.logo} alt={`${data.title}-Icon`} className={classes.img} />
+                <Typography className={classes.tokenName}> {" "} {data.title} </Typography>
+              </TableCell>
+              <TableCell align="right">
+                <span>
+                  {(data.marketSize > 0) ? decimalify(data.marketSize.toString(), decimals[data.title]) : "0"} {" "} {data.title}
+                </span> <br/>
+                <span className={classes.faintFont}>
+                  ${(data.marketSize > 0) ? decimalify((data.marketSize * data.usdPrice).toString(), decimals[data.title]) : "0.00"}
+                </span>
+              </TableCell>
+              <TableCell align="right">
+                <span>
+                  {(data.totalBorrowed > 0) ? decimalify(data.totalBorrowed.toString(), decimals[data.title]) : "0"} {" "} {data.title}
+                </span> <br/>
+                <span className={classes.faintFont}>
+                  ${(data.totalBorrowed > 0) ? decimalify((data.totalBorrowed * data.usdPrice).toString(), decimals[data.title]) : "0.00"}
+                </span>
+              </TableCell>
+              <TableCell align="right"> {(data.supplyRate > 0) ? Number(data.supplyRate).toFixed(2) : "0"}% </TableCell>
+              <TableCell align="right"> {(data.borrowRate > 0) ? Number(data.borrowRate).toFixed(2) : "0"}% </TableCell>
+              <TableCell align="right">
+                <span>
+                  {(data.walletBalance > 0) ? decimalify(data.walletBalance.toString(), decimals[data.title]) : "0"} {data.title}
+                </span> <br/>
+                <span className={classes.faintFont}>
+                  ${(data.walletBalance > 0) ? decimalify((data.walletBalance * data.usdPrice).toString(), decimals[data.title]) : "0.00"}
+                </span>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
 	);
 };
 
