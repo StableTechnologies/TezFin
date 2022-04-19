9c209125bd820e2af6586941a48bbd2e816d967d
Soumya Ghosh Dastidar <gdsoumya@gmail.com>
2022-04-19 19:13:54 +0530

    added seize token and related functions and storage vars
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>

--- contracts/errors/MathErrors.py
+++ contracts/errors/MathErrors.py
@@ -1,3 +1,4 @@
 class ErrorCodes:
     DIVISION_BY_ZERO = "DIVISION_BY_ZERO" # Division by zero
     SUBTRACTION_UNDERFLOW = "SUBTRACTION_UNDERFLOW" # Subtraction underflow
+    ADDITION_OVERFLOW = "ADDITION_OVERFLOW"  # Addition overflow
