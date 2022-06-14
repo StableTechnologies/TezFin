3eab2c414c06fc709cec340eebf6efc2aaece752
Siddharth Vaderaa <svaderaa@gmail.com>
2022-06-14 10:21:16 +0200

    Supply/Borrow Markets interfaces use bigInt

--- src/util/src/types.ts
+++ src/util/src/types.ts
@@ -32,7 +32,7 @@ export interface BorrowComposition {
     * @param
     */
 export interface SupplyMarket {
-    rate: BigNumber;
+    rate: bigInt.BigNumber;
     balanceUnderlying: BigNumber;
     balanceUsd: bigInt.BigInteger;
     collateral: boolean;
@@ -44,7 +44,7 @@ export interface SupplyMarket {
     * @param
     */
 export interface BorrowMarket {
-    rate: BigNumber;
+    rate: bigInt.BigInteger;
     balanceUnderlying: bigInt.BigInteger;
     balanceUsd: bigInt.BigInteger;
     liquidityUnderlying: bigInt.BigInteger;
@@ -59,7 +59,7 @@ export interface BorrowMarket {
     * @param borrowLimitUsed Percentage of account's borrow limit currently used
     */
 export interface SupplyMarketModal {
-    rate?: BigNumber;
+    rate?: bigInt.BigInteger;
     borrowLimitUsd: bigInt.BigInteger;
     borrowLimitUsed: number
 }
@@ -72,7 +72,7 @@ export interface SupplyMarketModal {
     * @param borrowLimitUsed Percentage of account's borrow limit currently used
     */
 export interface BorrowMarketModal {
-    rate: BigNumber;
+    rate: bigInt.BigInteger;
     borrowBalanceUsd: bigInt.BigInteger;
     borrowLimitUsed: number;
     borrowBalance: bigInt.BigInteger;
