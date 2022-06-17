9139ba4388507776ea8a5bf9a1612b7f4351a876
aeesh <adamaisha352@gmail.com>
2022-06-17 12:15:40 +0100

    fix reveal issue

--- src/ui/src/util/index.js
+++ src/ui/src/util/index.js
@@ -1,18 +1,16 @@
 import {
-	KeyStoreCurve,
-	KeyStoreType,
-	TezosNodeReader,
-	TezosNodeWriter,
-} from "conseiljs";
+    KeyStoreCurve,
+    KeyStoreType,
+    TezosNodeReader,
+    TezosNodeWriter
+} from 'conseiljs';
 
-import { BigNumber } from "bignumber.js";
-import { DAppClient } from "@airgap/beacon-sdk";
-import bigInt from "big-integer";
+import { BigNumber } from 'bignumber.js';
+import { DAppClient } from '@airgap/beacon-sdk';
+import bigInt from 'big-integer';
 
-// const config = require(`../library/${process.env.REACT_APP_ENV || "prod"}-network-config.json`);
-const config = require(`../library/${
-	process.env.REACT_APP_ENV || "prod"
-}-network-config.json`);
+// eslint-disable-next-line import/no-dynamic-require
+const config = require(`../library/${process.env.REACT_APP_ENV || 'prod'}-network-config.json`);
 
 const client = new DAppClient({ name: config.dappName });
 
@@ -24,8 +22,7 @@ const client = new DAppClient({ name: config.dappName });
  * @param {string} str Address to format.
  * @returns
  */
-export const shorten = (first, last, str) =>
-	`${str.substring(0, first)}...${str.substring(str.length - last)}`;
+export const shorten = (first, last, str) => `${str.substring(0, first)}...${str.substring(str.length - last)}`;
 
 /**
  * This function lets a user to connect to a tezos wallet.
@@ -33,22 +30,22 @@ export const shorten = (first, last, str) =>
  * @returns clients
  */
 export const getWallet = async () => {
-	const { network } = config.infra.conseilServer;
-	const response = await client.requestPermissions({
-		network: { type: network },
-	});
-	const { address } = response;
+    const { network } = config.infra.conseilServer;
+    const response = await client.requestPermissions({
+        network: { type: network }
+    });
+    const { address } = response;
 
-	return { address };
+    return { address };
 };
 
 /**
  * This function let's a user disconnects from an account.
  */
 export const deactivateAccount = async () => {
-	await client.clearActiveAccount();
-	// eslint-disable-next-line no-undef
-	localStorage.clear();
+    await client.clearActiveAccount();
+    // eslint-disable-next-line no-undef
+    localStorage.clear();
 };
 
 /**
@@ -57,8 +54,8 @@ export const deactivateAccount = async () => {
  * @returns address
  */
 export const getActiveAccount = async () => {
-	const activeAccount = await client.getActiveAccount();
-	return activeAccount ? activeAccount.address : undefined;
+    const activeAccount = await client.getActiveAccount();
+    return activeAccount ? activeAccount.address : undefined;
 };
 
 /**
@@ -69,39 +66,41 @@ export const getActiveAccount = async () => {
  * @return operation group.
  */
 export const evaluateTransaction = async (operations) => {
-	try {
-		const address = operations[0].source;
-		let curve = KeyStoreCurve.ED25519;
-		if (address.startsWith("tz2")) {
-			curve = KeyStoreCurve.SECP256K1;
-		} else if (address.startsWith("tz3")) {
-			curve = KeyStoreCurve.SECP256R1;
-		}
-		const keyStore = {
-			publicKey: "", // this precludes reveal operation inclusion
-			secretKey: "",
-			publicKeyHash: address,
-			curve,
-			storeType: KeyStoreType.Mnemonic,
-		};
+    try {
+        const address = operations[0].source;
+        let curve = KeyStoreCurve.ED25519;
+        if (address.startsWith('tz2')) {
+            curve = KeyStoreCurve.SECP256K1;
+        } else if (address.startsWith('tz3')) {
+            curve = KeyStoreCurve.SECP256R1;
+        }
 
-		const counter = await TezosNodeReader.getCounterForAccount(
-			config.infra.tezosNode,
-			address
-		);
+        const activeAccount = await client.getActiveAccount();
+        const keyStore = {
+            publicKey: activeAccount.publicKey,
+            secretKey: '',
+            publicKeyHash: address,
+            curve,
+            storeType: KeyStoreType.Mnemonic
+        };
 
-		const opGroup = await TezosNodeWriter.prepareOperationGroup(
-			config.infra.tezosNode,
-			keyStore,
-			counter,
-			operations,
-			true
-		);
-		return { opGroup };
-	} catch (error) {
-		console.log("evaluateTX", error);
-		return { error };
-	}
+        const counter = await TezosNodeReader.getCounterForAccount(
+            config.infra.tezosNode,
+            address
+        );
+
+        const opGroup = await TezosNodeWriter.prepareOperationGroup(
+            config.infra.tezosNode,
+            keyStore,
+            counter,
+            operations,
+            true
+        );
+        return { opGroup };
+    } catch (error) {
+        console.log('evaluateTX', error);
+        return { error };
+    }
 };
 /**
  * Sends a transaction to the blockchain
@@ -111,16 +110,16 @@ export const evaluateTransaction = async (operations) => {
  * @return operation response
  */
 export const confirmTransaction = async (opGroup) => {
-	try {
-		const head = await TezosNodeReader.getBlockHead(config.infra.tezosNode);
-		const response = await client.requestOperation({
-			operationDetails: opGroup,
-		});
-		return { response: { response, head: head.header.level } };
-	} catch (error) {
-		console.log(error);
-		return { error };
-	}
+    try {
+        const head = await TezosNodeReader.getBlockHead(config.infra.tezosNode);
+        const response = await client.requestOperation({
+            operationDetails: opGroup
+        });
+        return { response: { response, head: head.header.level } };
+    } catch (error) {
+        console.log(error);
+        return { error };
+    }
 };
 
 /**
@@ -131,28 +130,28 @@ export const confirmTransaction = async (opGroup) => {
  * @return operation response
  */
 export const verifyTransaction = async ({ response, head }) => {
-	try {
-		const groupid = response.transactionHash
-			.replace(/"/g, "")
-			.replace(/\n/, ""); // clean up RPC output
-		const confirm = await TezosNodeReader.awaitOperationConfirmation(
-			config.infra.tezosNode,
-			head - 1,
-			groupid,
-			6
-		).then((res) => {
-			if (
-				res.contents[0].metadata.operation_result.status === "applied"
-			) {
-				return res;
-			}
-			throw new Error("operation status not applied");
-		});
-		return { confirm };
-	} catch (error) {
-		console.log(error);
-		return { error };
-	}
+    try {
+        const groupid = response.transactionHash
+            .replace(/"/g, '')
+            .replace(/\n/, ''); // clean up RPC output
+        const confirm = await TezosNodeReader.awaitOperationConfirmation(
+            config.infra.tezosNode,
+            head - 1,
+            groupid,
+            6
+        ).then((res) => {
+            if (
+                res.contents[0].metadata.operation_result.status === 'applied'
+            ) {
+                return res;
+            }
+            throw new Error('operation status not applied');
+        });
+        return { confirm };
+    } catch (error) {
+        console.log(error);
+        return { error };
+    }
 };
 
 /**
@@ -161,15 +160,15 @@ export const verifyTransaction = async ({ response, head }) => {
  * @returns decimal version
  */
 export const decimalify = (val, decimals, formatDecimals = 4) => {
-	if (!val) {
-		return val;
-	}
+    if (!val) {
+        return val;
+    }
 
-	return Number(
-		new BigNumber(val.toString())
-			.div(new BigNumber(10).pow(new BigNumber(decimals.toString())))
-			.toFixed(formatDecimals)
-	);
+    return Number(
+        new BigNumber(val.toString())
+            .div(new BigNumber(10).pow(new BigNumber(decimals.toString())))
+            .toFixed(formatDecimals)
+    );
 };
 
 /**
@@ -178,21 +177,21 @@ export const decimalify = (val, decimals, formatDecimals = 4) => {
  * @returns decimal version
  */
 export const undecimalify = (val, decimals) => {
-	if (!val) {
-		return val;
-	}
+    if (!val) {
+        return val;
+    }
 
-	return new BigNumber(val.toString())
-		.multipliedBy(new BigNumber(10).pow(new BigNumber(decimals.toString())))
-		.toFixed(0);
+    return new BigNumber(val.toString())
+        .multipliedBy(new BigNumber(10).pow(new BigNumber(decimals.toString())))
+        .toFixed(0);
 };
 
 /**
  * Format token data for display in the market table.
  */
 export function formatTokenData(data) {
-	const filtered = data.filter((i) => bigInt(i.balanceUnderlying).gt(0));
-	return filtered;
+    const filtered = data.filter((i) => bigInt(i.balanceUnderlying).gt(0));
+    return filtered;
 }
 
 /**
@@ -201,8 +200,7 @@ export function formatTokenData(data) {
  *
  * @return truncated value.
  */
-export const truncateNum = (num) =>
-	num.toString().match(/^-?\d+(?:\.\d{0,2})?/);
+export const truncateNum = (num) => num.toString().match(/^-?\d+(?:\.\d{0,2})?/);
 
 /**
  * This function abbreviates a number and returns it as a string with it's suffix.
@@ -211,27 +209,24 @@ export const truncateNum = (num) =>
  * @returns abbreviated number in string format.
  */
 export const nFormatter = (num, formatDecimals = 4) => {
-	const suffix = [
-		{ value: 1, symbol: "" },
-		{ value: 1e3, symbol: "k" },
-		{ value: 1e6, symbol: "M" },
-		{ value: 1e9, symbol: "B" },
-	];
-	let i;
-	// eslint-disable-next-line no-plusplus
-	for (i = suffix.length - 1; i > 0; i--) {
-		if (num >= suffix[i].value) {
-			break;
-		}
-	}
+    const suffix = [
+        { value: 1, symbol: '' }, { value: 1e3, symbol: 'k' }, { value: 1e6, symbol: 'M' }, { value: 1e9, symbol: 'B' }
+    ];
+    let i;
+    // eslint-disable-next-line no-plusplus
+    for (i = suffix.length - 1; i > 0; i--) {
+        if (num >= suffix[i].value) {
+            break;
+        }
+    }
 
-	let formattedNum = new BigNumber(num).dividedBy(suffix[i].value).toString();
-	if (formattedNum % 1 !== 0) {
-		formattedNum = +formattedNum.slice(
-			0,
-			formattedNum.toString().indexOf(".") + (formatDecimals + 1)
-		);
-	}
+    let formattedNum = new BigNumber(num).dividedBy(suffix[i].value).toString();
+    if (formattedNum % 1 !== 0) {
+        formattedNum = +formattedNum.slice(
+            0,
+            formattedNum.toString().indexOf('.') + (formatDecimals + 1)
+        );
+    }
 
-	return formattedNum + suffix[i].symbol;
+    return formattedNum + suffix[i].symbol;
 };
