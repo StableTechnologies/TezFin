eca651f78910dab7438825d792ded8002246fc7c
Siddharth Vaderaa <svaderaa@gmail.com>
2022-06-21 10:41:32 +0200

    Contract test modified
    Earlier we were testing for apy and extracting the borrow rate from it .
    Now the tests directly check supply and borrow rates. Apy test cases
    removed from the contract

--- contracts/tests/JsInterestRateTest.py
+++ contracts/tests/JsInterestRateTest.py
@@ -4,7 +4,11 @@ IRM = sp.io.import_script_from_url("file:contracts/InterestRateModel.py")
 RV = sp.io.import_script_from_url("file:contracts/tests//utils/ResultViewer.py")
 
 
-@sp.add_test(name = "Apy_Js_Implementation_Test_Cases")
+# These test cases have been added to confirm that the Js Implementation of 
+# BorrowRate and SupplyRate give the identical number for all significant digits
+# for the inputs given below
+
+@sp.add_test(name = "Borrow_and_Supply_Rate_Js_Implementation_Test_Cases")
 def test():
     expScale = sp.nat(int(1e18))
 
@@ -15,7 +19,7 @@ def test():
     scenario.add_flag("protocol", "ithaca")
 
     scenario.table_of_contents()
-    scenario.h1("Test cases from the tests in the  JS implemetation for  APY rate,  in commit 6a554d3819e7d950a62a2d4a10a6eddaa08f26e5")
+    scenario.h1("Test cases from the tests in the  JS implemetation for Supply and Borrow  rate")
 
     annualPeriods = 1051920
     baseRatePerBlock = 950642634 
@@ -28,11 +32,11 @@ def test():
     view_result = RV.ViewerNat()
     scenario += view_result
 
-    scenario.h2("Test cases, to test if the JS implemetation for calculating APY uses the borrow rate  that is equivelant to the one in the contract for the inputs below ")
+    scenario.h2("Test cases, to test if the JS implemetation for supply and t borrow rate is equivelant to the one in the contract for the market states below ")
 
     scenario.h3("Market State at 1k lent, 1 borrowed")
 
-    scenario.h4("Calculate BorrowRate From APY")
+    scenario.h4("Calculate BorrowRate")
     scenario += c2.getBorrowRate(sp.record(cash=sp.nat(1000000000000000000000), 
                                            borrows=sp.nat(1000000000000000000), 
                                            reserves=sp.nat(0), 
@@ -41,22 +45,22 @@ def test():
     # the APY rates are in decimal form in JS we have to convert them back into
     # mantissa form  for use in tests 
 
-    jsApyBorrowRateMatissa =  1048951048368960 #.00104895104836896
-    scenario.verify_equal(view_result.data.last , sp.some(jsApyBorrowRateMatissa // annualPeriods))
+    jsBorrowRateMantissa =  997177588 
+    scenario.verify_equal(view_result.data.last , sp.some(jsBorrowRateMantissa))
 
-    scenario.h3("Calculate SupplyRate From APY")
+    scenario.h3("Calculate SupplyRate")
     scenario += c2.getSupplyRate(sp.record(cash=sp.nat(1000000000000000000000),
                                            borrows=sp.nat(1000000000000000000),
                                            reserves=sp.nat(0), 
                                            reserveFactorMantissa=reserveFactorMantissa, 
                                            cb=view_result.typed.targetNat))
      
-    jsApySupplyRateMatissa = 1046855005200 #.000001046855005200
-    scenario.verify_equal(view_result.data.last, sp.some(jsApySupplyRateMatissa // annualPeriods))
+    jsSupplyRateMantissa = 995185 
+    scenario.verify_equal(view_result.data.last, sp.some(jsSupplyRateMantissa))
 
     scenario.h3("Market State at 1k lent, 10 borrowed")
 
-    scenario.h4("Calculate BorrowRate From APY")
+    scenario.h4("Calculate BorrowRate")
     scenario += c2.getBorrowRate(sp.record(cash=sp.nat(1000000000000000000000), 
                                            borrows=sp.nat(10000000000000000000), 
                                            reserves=sp.nat(0), 
@@ -65,24 +69,24 @@ def test():
     # the APY rates are in decimal form in JS we have to convert them back into
     # mantissa form  for use in tests 
 
-    jsApyBorrowRateMatissa =  1485148514152320       # .001485148514152320 
-    scenario.verify_equal(view_result.data.last , sp.some(jsApyBorrowRateMatissa // annualPeriods))
+    jsBorrowRateMantissa =  1411845496 
+    scenario.verify_equal(view_result.data.last , sp.some(jsBorrowRateMantissa))
 
     
-    scenario.h3("Calculate SupplyRate From APY")
+    scenario.h3("Calculate SupplyRate")
     scenario += c2.getSupplyRate(sp.record(cash=sp.nat(1000000000000000000000),
                                            borrows=sp.nat(10000000000000000000),
                                            reserves=sp.nat(0), 
                                            reserveFactorMantissa=reserveFactorMantissa, 
                                            cb=view_result.typed.targetNat))
      
-    jsApySupplyRateMatissa = 14689735652880           #.001468973565288000
-    scenario.verify_equal(view_result.data.last, sp.some(jsApySupplyRateMatissa // annualPeriods))
+    jsSupplyRateMantissa = 13964689
+    scenario.verify_equal(view_result.data.last, sp.some(jsSupplyRateMantissa))
 
 
     scenario.h3("Market State at 1k lent , 100 borrowed")
 
-    scenario.h4("Calculate BorrowRate From APY")
+    scenario.h4("Calculate BorrowRate")
     scenario += c2.getBorrowRate(sp.record(cash=sp.nat(1000000000000000000000), 
                                            borrows=sp.nat(100000000000000000000), 
                                            reserves=sp.nat(0), 
@@ -91,23 +95,23 @@ def test():
     # the APY rates are in decimal form in JS we have to convert them back into
     # mantissa form  for use in tests 
 
-    jsApyBorrowRateMatissa =  5454545452991280       # .005454545452991280 
-    scenario.verify_equal(view_result.data.last , sp.some(jsApyBorrowRateMatissa // annualPeriods))
+    jsBorrowRateMantissa =  5185323459 
+    scenario.verify_equal(view_result.data.last , sp.some(jsBorrowRateMantissa))
 
     
-    scenario.h3("Calculate SupplyRate From APY")
+    scenario.h3("Calculate SupplyRate")
     scenario += c2.getSupplyRate(sp.record(cash=sp.nat(1000000000000000000000),
                                            borrows=sp.nat(100000000000000000000),
                                            reserves=sp.nat(0), 
                                            reserveFactorMantissa=reserveFactorMantissa, 
                                            cb=view_result.typed.targetNat))
      
-    jsApySupplyRateMatissa = 495371899964160        #.000495371899964160
-    scenario.verify_equal(view_result.data.last, sp.some(jsApySupplyRateMatissa // annualPeriods))
+    jsSupplyRateMantissa = 470921648
+    scenario.verify_equal(view_result.data.last, sp.some(jsSupplyRateMantissa))
 
     scenario.h3("Market State at 1k lent , 1k borrowed")
 
-    scenario.h4("Calculate BorrowRate From APY")
+    scenario.h4("Calculate BorrowRate")
     scenario += c2.getBorrowRate(sp.record(cash=sp.nat(1000000000000000000000), 
                                            borrows=sp.nat(1000000000000000000000), 
                                            reserves=sp.nat(0), 
@@ -116,18 +120,18 @@ def test():
     # the APY rates are in decimal form in JS we have to convert them back into
     # mantissa form  for use in tests 
 
-    jsApyBorrowRateMatissa =  25499999999229840       # .025499999999229840 
-    scenario.verify_equal(view_result.data.last , sp.some(jsApyBorrowRateMatissa // annualPeriods))
+    jsBorrowRateMantissa =  24241387177 
+    scenario.verify_equal(view_result.data.last , sp.some(jsBorrowRateMantissa))
 
     
-    scenario.h3("Calculate SupplyRate From APY")
+    scenario.h3("Calculate SupplyRate")
     scenario += c2.getSupplyRate(sp.record(cash=sp.nat(1000000000000000000000),
                                            borrows=sp.nat(1000000000000000000000),
                                            reserves=sp.nat(0), 
                                            reserveFactorMantissa=reserveFactorMantissa, 
                                            cb=view_result.typed.targetNat))
      
-    jsApySupplyRateMatissa = 12737249998656480           #.012737249998656480#
-    scenario.verify_equal(view_result.data.last, sp.some(jsApySupplyRateMatissa // annualPeriods))
+    jsSupplyRateMantissa = 12108572894
+    scenario.verify_equal(view_result.data.last, sp.some(jsSupplyRateMantissa))
 
 
