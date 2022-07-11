6bb655d999cc7b2276cf3f27a167091d3e57adf9
Siddharth Vaderaa <svaderaa@gmail.com>
2022-07-11 14:19:36 +0200

    BorrowRateMaxMantissa set in CToken
    This parameter was set in CToken as a higher than expected guarantee that
    the contract can't breach. Modeled for borrowAPY of approx 8.5%
    
    borrowratemaxmantissa=80000000000

--- contracts/CToken.py
+++ contracts/CToken.py
@@ -29,7 +29,7 @@ class CToken(CTI.CTokenInterface, Exponential.Exponential, SweepTokens.SweepToke
                 balance=sp.TNat)),  # Official record of token balances for each account
             totalSupply=sp.nat(0),  # Total number of tokens in circulation
             # Maximum borrow rate that can ever be applied (.0000000475% / block)
-            borrowRateMaxMantissa=sp.nat(int(47532131721)),
+            borrowRateMaxMantissa=sp.nat(int(80000000000)),
             # Maximum fraction of interest that can be set aside for reserves
             reserveFactorMaxMantissa=sp.nat(int(1e18)),
             comptroller=comptroller_,  # Contract which oversees inter-cToken operations
