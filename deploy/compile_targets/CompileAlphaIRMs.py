ad0e63b802a03c69365d7560f06e907e3c398e2e
hugobucquet <hugo@bucquet.com>
2022-07-27 10:55:26 -0400

    minor bug

--- deploy/compile_targets/CompileAlphaIRMs.py
+++ deploy/compile_targets/CompileAlphaIRMs.py
@@ -4,21 +4,21 @@ IRM = sp.io.import_script_from_url("file:contracts/AlphaInterestRateModel.py")
 CFG = sp.io.import_script_from_url("file:deploy/compile_targets/Config.py")
 
 sp.add_compilation_target("CFA12_IRM", IRM.InterestRateModel(
-    scale_ = sp.nat(1e18),
+    scale_ = sp.nat(int(1e18)),
     multiplierPerBlock_ = CFG.CFA12_IRM.multiplierPerBlock,
     baseRatePerBlock_ = CFG.CFA12_IRM.baseRatePerBlock,
     alpha_ = CFG.ALPHA_CFA12_IRM.alpha,
     admin_ = ""))
 
 sp.add_compilation_target("CFA2_IRM", IRM.InterestRateModel(
-    scale_ = sp.nat(1e18),
+    scale_ = sp.nat(int(1e18)),
     multiplierPerBlock_ = CFG.CFA2_IRM.multiplierPerBlock,
     baseRatePerBlock_ = CFG.CFA2_IRM.baseRatePerBlock,
     alpha_ = CFG.ALPHA_CFA2_IRM.alpha,
     admin_ = ""))
 
 sp.add_compilation_target("CXTZ_IRM", IRM.InterestRateModel(
-    scale_ = sp.nat(1e18),
+    scale_ = sp.nat(int(1e18)),
     multiplierPerBlock_ = CFG.CXTZ_IRM.multiplierPerBlock,
     baseRatePerBlock_ = CFG.CXTZ_IRM.baseRatePerBlock,
     alpha_ = CFG.ALPHA_CXTZ_IRM.alpha,
