3f697c0d8eaa1bcd3c2fe9fb13f5a3077e37abc3
aeesh <adamaisha352@gmail.com>
2022-08-30 16:09:53 +0100

    added wtz and oxtz logos

--- src/ui/src/components/Constants/index.js
+++ src/ui/src/components/Constants/index.js
@@ -3,6 +3,8 @@ import BTCtz from '../../assets/BTCtez.svg';
 import Ethtz from '../../assets/ETHtz.svg';
 import USDtz from '../../assets/USDtz.svg';
 import XTZ from '../../assets/XTZ.svg';
+import wtz from '../../assets/wtz.svg';
+import oxtz from '../../assets/oXTZ.png';
 // import ctez from '../../assets/ctez.svg';
 // import kUSD from '../../assets/kUSD.svg';
 import fBTCtz from '../../assets/fbtctz.svg';
@@ -10,6 +12,7 @@ import fEthtz from '../../assets/fethtz.svg';
 import fUSDtz from '../../assets/fusdtz.svg';
 import fXTZ from '../../assets/fXTZ.svg';
 // import fCtez from '../../assets/fctez.svg';
+import fOXTZ from '../../assets/foxtz.svg';
 
 export const tokens = [
     {
@@ -21,26 +24,26 @@ export const tokens = [
     {
         title: 'XTZ', logo: XTZ, fLogo: fXTZ, banner: 'Tez', assetType: AssetType.XTZ
     },
-    //   { title: 'kUSD', logo: kUSD, banner: 'kUSD', assetType: '' },
-    //   { title: 'CTez', logo: ctez, banner: 'CTez', assetType: '' },
     {
         title: 'USDtz', logo: USDtz, fLogo: fUSDtz, banner: 'USDtz', assetType: AssetType.USD
     },
+    //   { title: 'kUSD', logo: kUSD, banner: 'kUSD', assetType: '' },
+    // {
+    //     title: 'CTez', logo: ctez, fLogo: fCtez, banner: 'CTez', assetType: AssetType.CTEZ
+    // },
     {
-        title: 'oXTZ', logo: USDtz, fLogo: fUSDtz, banner: 'oXTZ', assetType: AssetType.OXTZ
+        title: 'OXTZ', logo: oxtz, fLogo: fOXTZ, banner: 'OXTZ', assetType: AssetType.OXTZ
     },
     {
-        title: 'WTZ', logo: USDtz, fLogo: fUSDtz, banner: 'WTZ', assetType: AssetType.WTZ
+        title: 'WTZ', logo: wtz, fLogo: wtz, banner: 'WTZ', assetType: AssetType.WTZ // TODO:  update fToken for  WTZ
     }
 ];
 export const tokenColor = {
     XTZ: '#3391F6',
-    ETH: '#662F9D',
-    BTC: '#F2991A',
-    USD: '#189DA3',
     ETHtz: '#662F9D',
     BTCtz: '#F2991A',
     USDtz: '#189DA3',
-    oXTZ : '#189DA3',
-    WTZ: '#189DA3'
+    oXTZ: '#B52B31',
+    WTZ: '#0C2C93'
+    // CTEZ: '#2B62F8'
 };
