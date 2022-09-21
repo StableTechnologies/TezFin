fa9c2e05197fccbce52f9eed440ea6d4b6a5fc71
Siddharth Vaderaa <svaderaa@gmail.com>
2022-09-21 14:46:21 +0530

    Updated protocol to jakarta

--- deploy/shell_scripts/deploy_cfa12.sh
+++ deploy/shell_scripts/deploy_cfa12.sh
@@ -4,11 +4,11 @@ set -e # Any subsequent(*) commands which fail will cause the shell script to ex
 # example:  ./deploy/shell_scripts/deploy_cfa12.sh ~/smartpy-cli/SmartPy.sh
 
 
-($1 compile ./deploy/compile_targets/CompileCFA12_IRM.py ./TezFinBuild/compiled_contracts --purge --protocol granada)\
+($1 compile ./deploy/compile_targets/CompileCFA12_IRM.py ./TezFinBuild/compiled_contracts --purge --protocol jakarta)\
 && echo "CompileCFA12_IRM.py was successfully compiled" || echo
 node ./deploy/deploy_script/deploy.js
 
 
-($1 compile ./deploy/compile_targets/CompileCFA12.py ./TezFinBuild/compiled_contracts --purge --protocol granada --erase-comments --erase-var-annots --initial-cast)\
+($1 compile ./deploy/compile_targets/CompileCFA12.py ./TezFinBuild/compiled_contracts --purge --protocol jakarta --erase-comments --erase-var-annots --initial-cast)\
 && echo "CompileCFA12.py was successfully compiled" || echo
 node ./deploy/deploy_script/deploy.js
