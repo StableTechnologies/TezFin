da5dc4e66eb44e63a517010829ed998f122da2ce
Siddharth Vaderaa <svaderaa@gmail.com>
2024-02-08 11:26:07 +0100

    Fix edge case borrow rate discrepancy
    The UI now applies  zero interest when max rate is breached.

--- src/util/src/FToken.ts
+++ src/util/src/FToken.ts
@@ -427,7 +427,8 @@ export namespace FToken {
         const _blockRate = getBorrowRate(storage, irStorage);
 
         if (_blockRate.greaterOrEquals(storage.borrow.borrowRateMaxMantissa)) {
-            return _calcAnnualizedRate(storage.borrow.borrowRateMaxMantissa, irStorage.scale).multiply(100);
+            // return bigint 0 when borrow rate is greater than max borrow rate
+            return bigInt(0);
         }
 
         return _calcAnnualizedRate(_blockRate, irStorage.scale).multiply(100);
