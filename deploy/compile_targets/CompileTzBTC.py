897f35012a243913ef8935cac0d31c21fe0e0d0c
ilya.razievskij <ilya.razievskij@gmail.com>
2025-04-16 12:05:47 +0300

    chore: add metadata for v3 markets

--- deploy/compile_targets/CompileTzBTC.py
+++ deploy/compile_targets/CompileTzBTC.py
@@ -16,19 +16,18 @@ sp.add_compilation_target("CtzBTC", CFA12.CFA12(
     metadata_ = sp.big_map({
         "": sp.utils.bytes_of_string("tezos-storage:data"),
         "data": sp.utils.bytes_of_string(json.dumps({
-            "name": "...",
-            "description": "...",
-            "version": "1.0.0",
-            "authors": ["..."],
-            "homepage": "https://some-website.com",
-            "interfaces": ["TZIP-007"],
-            "license": {"name": "..."}
+            "name": "TezFin Interest-Bearing tzBTC",
+            "description": "Interest-bearing token for tzBTC (wrapped Bitcoin on Tezos) supplied to the TezFin lending protocol.",
+            "version": "3.0",
+            "authors": ["Tezos Finance Protocol"],
+            "homepage": "https://tezos.finance",
+            "interfaces": ["TZIP-007", "TZIP-016"],
         }))
     }),
     token_metadata_ = {
-        "name": sp.utils.bytes_of_string("..."),
-        "symbol": sp.utils.bytes_of_string("..."),
-        "decimals": sp.utils.bytes_of_string("..."),
+        "name": sp.utils.bytes_of_string("TezFin Interest-Bearing tzBTC"),
+        "symbol": sp.utils.bytes_of_string("\ua730tzBTC"),
+        "decimals": sp.utils.bytes_of_string("8"),
     },
     fa1_2_TokenAddress_ = sp.address(CFG.deployResult.tzBTC)
     ))
