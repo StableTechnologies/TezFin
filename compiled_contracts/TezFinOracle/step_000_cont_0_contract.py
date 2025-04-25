845cf8bf1ea024f713fa067b6f2e9ec3ce6e4954
ilya.razievskij <ilya.razievskij@gmail.com>
2025-04-25 11:47:16 +0300

    chore: updated compiled contracts

--- compiled_contracts/TezFinOracle/step_000_cont_0_contract.py
+++ compiled_contracts/TezFinOracle/step_000_cont_0_contract.py
@@ -4,48 +4,48 @@ class Contract(sp.Contract):
   def __init__(self):
     self.init_type(sp.TRecord(admin = sp.TAddress, alias = sp.TBigMap(sp.TString, sp.TString), oracle = sp.TAddress, overrides = sp.TBigMap(sp.TString, sp.TPair(sp.TTimestamp, sp.TNat)), pendingAdmin = sp.TOption(sp.TAddress)).layout((("admin", "alias"), ("oracle", ("overrides", "pendingAdmin")))))
     self.init(admin = sp.address('tz1RESHvJAfmQCXCAD3ubNmVtac788pnN1oL'),
-              alias = {'OXTZ-USD' : 'XTZ-USD', 'WTZ-USD' : 'XTZ-USD'},
-              oracle = sp.address('KT1KBrn1udLLrGNbQ3n1mWgMVXkr26krj6Nj'),
-              overrides = {'USD-USD' : (sp.timestamp(1697180304), 1000000)},
+              alias = {'OXTZ-USD' : 'XTZ-USD', 'TZBTC-USD' : 'BTC-USD', 'WTZ-USD' : 'XTZ-USD'},
+              oracle = sp.address('KT1B74ywpG3nX2F3ZgpYi4MnsyuoYyQtQTAn'),
+              overrides = {'USD-USD' : (sp.timestamp(1745570166), 1000000), 'USDT-USD' : (sp.timestamp(1745570166), 1000000)},
               pendingAdmin = sp.none)
 
-  @sp.entry_point
+  @sp.entrypoint
   def accept_admin(self):
     sp.verify(sp.sender == self.data.pendingAdmin.open_some(message = 'NOT_SET_PENDING_ADMIN'), 'NOT_PENDING_ADMIN')
     self.data.admin = sp.sender
     self.data.pendingAdmin = sp.none
 
-  @sp.entry_point
+  @sp.entrypoint
   def addAlias(self, params):
     sp.verify(sp.sender == self.data.admin, 'NOT_ADMIN')
     sp.set_type(params, sp.TList(sp.TRecord(alias = sp.TString, asset = sp.TString).layout(("alias", "asset"))))
     sp.for item in params:
       self.data.alias[item.alias] = item.asset
 
-  @sp.entry_point
+  @sp.entrypoint
   def removeAlias(self, params):
     sp.verify(sp.sender == self.data.admin, 'NOT_ADMIN')
     del self.data.alias[params]
 
-  @sp.entry_point
+  @sp.entrypoint
   def removeAsset(self, params):
     sp.verify(sp.sender == self.data.admin, 'NOT_ADMIN')
     del self.data.overrides[params]
 
-  @sp.entry_point
+  @sp.entrypoint
   def setPrice(self, params):
     sp.verify(sp.sender == self.data.admin, 'NOT_ADMIN')
     sp.set_type(params, sp.TList(sp.TRecord(asset = sp.TString, price = sp.TNat).layout(("asset", "price"))))
     sp.for item in params:
       self.data.overrides[item.asset] = (sp.now, item.price)
 
-  @sp.entry_point
+  @sp.entrypoint
   def set_oracle(self, params):
     sp.set_type(params, sp.TAddress)
     sp.verify(sp.sender == self.data.admin, 'NOT_ADMIN')
     self.data.oracle = params
 
-  @sp.entry_point
+  @sp.entrypoint
   def set_pending_admin(self, params):
     sp.set_type(params, sp.TAddress)
     sp.verify(sp.sender == self.data.admin, 'NOT_ADMIN')
--- compiled_contracts/TezFinOracle/step_000_cont_0_types.py
+++ compiled_contracts/TezFinOracle/step_000_cont_0_types.py
@@ -3,4 +3,4 @@ import smartpy as sp
 tstorage = sp.TRecord(admin = sp.TAddress, alias = sp.TBigMap(sp.TString, sp.TString), oracle = sp.TAddress, overrides = sp.TBigMap(sp.TString, sp.TPair(sp.TTimestamp, sp.TNat)), pendingAdmin = sp.TOption(sp.TAddress)).layout((("admin", "alias"), ("oracle", ("overrides", "pendingAdmin"))))
 tparameter = sp.TVariant(accept_admin = sp.TUnit, addAlias = sp.TList(sp.TRecord(alias = sp.TString, asset = sp.TString).layout(("alias", "asset"))), removeAlias = sp.TString, removeAsset = sp.TString, setPrice = sp.TList(sp.TRecord(asset = sp.TString, price = sp.TNat).layout(("asset", "price"))), set_oracle = sp.TAddress, set_pending_admin = sp.TAddress).layout((("accept_admin", ("addAlias", "removeAlias")), (("removeAsset", "setPrice"), ("set_oracle", "set_pending_admin"))))
 tprivates = { }
-tviews = { "getPrice": (sp.TString, sp.TPair(sp.TTimestamp, sp.TNat)) }
+tviews = { "getPrice": (sp.TString, sp.TPair(sp.TTimestamp, sp.TNat)), "get_price_with_timestamp": (sp.TString, sp.TPair(sp.TNat, sp.TTimestamp)) }
